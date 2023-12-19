9b968bf1996a82db4e77fed6c4c113588ceeb632
Rylie Weaver <rylie@RylieLaptop>
2023-12-19 15:13:42 -0800

    IMPROVE_DATA_DIR instead of CANDLE_DATA_DIR

--- Pilot1/Uno_IMPROVE/preprocess.sh
+++ Pilot1/Uno_IMPROVE/preprocess.sh
@@ -4,7 +4,7 @@
 ### THIS IS A TEMPLATE FILE. SUBSTITUTE #PATH# WITH THE MODEL EXECUTABLE.
 #########################################################################
 
-# arg 1 CANDLE_DATA_DIR
+# arg 1 IMPROVE_DATA_DIR
 # arg 2 CANDLE_CONFIG
 
 ### Path and Name to your CANDLEized model's main Python script###
@@ -26,22 +26,22 @@ fi
 
 if [ $# -lt 2 ] ; then
         echo "Illegal number of parameters"
-        echo "CANDLE_DATA_DIR PARAMS are required"
+        echo "IMPROVE_DATA_DIR PARAMS are required"
         exit -1
 fi
 
 if [ $# -eq 2 ] ; then
-        CANDLE_DATA_DIR=$1 ; shift
+        IMPROVE_DATA_DIR=$1 ; shift
         CONFIG_FILE=$1 ; shift
         CMD="python ${CANDLE_MODEL} --config_file ${CONFIG_FILE}"
         echo "CMD = $CMD"
 
 elif [ $# -ge 3 ] ; then
         CUDA_VISIBLE_DEVICES=$1 ; shift
-        CANDLE_DATA_DIR=$1 ; shift
+        IMPROVE_DATA_DIR=$1 ; shift
 
         # if $3 is a file, then set candle_config
-        if [ -f $CANDLE_DATA_DIR/$1 ] ; then
+        if [ -f $IMPROVE_DATA_DIR/$1 ] ; then
 		echo "$1 is a file"
                 CANDLE_CONFIG=$1 ; shift
                 CMD="python ${CANDLE_MODEL} --config_file $CANDLE_CONFIG $@"
@@ -57,10 +57,10 @@ elif [ $# -ge 3 ] ; then
 fi
 
 # Display runtime arguments
-echo "using CANDLE_DATA_DIR ${CANDLE_DATA_DIR}"
+echo "using IMPROVE_DATA_DIR ${IMPROVE_DATA_DIR}"
 echo "using CANDLE_CONFIG ${CANDLE_CONFIG}"
 echo "running command ${CMD}"
 
 # Set up environmental variables and execute model
 # source /opt/conda/bin/activate /usr/local/conda_envs/Paccmann_MCA
-CANDLE_DATA_DIR=${CANDLE_DATA_DIR} $CMD
+IMPROVE_DATA_DIR=${IMPROVE_DATA_DIR} $CMD
--- Pilot1/Uno_IMPROVE/train.sh
+++ Pilot1/Uno_IMPROVE/train.sh
@@ -5,7 +5,7 @@
 #########################################################################
 
 # arg 1 CUDA_VISIBLE_DEVICES
-# arg 2 CANDLE_DATA_DIR
+# arg 2 IMPROVE_DATA_DIR
 # arg 3 CANDLE_CONFIG
 
 ### Path and Name to your CANDLEized model's main Python script###
@@ -27,22 +27,22 @@ fi
 if [ $# -lt 2 ] ; then
         echo "Illegalnumber of paramaters"
         echo "Illegal number of parameters"
-        echo "CUDA_VISIBLE_DEVICES CANDLE_DATA_DIR are required"
+        echo "CUDA_VISIBLE_DEVICES IMPROVE_DATA_DIR are required"
         exit -1
 fi
 
 if [ $# -eq 2 ] ; then
         CUDA_VISIBLE_DEVICES=$1 ; shift
-        CANDLE_DATA_DIR=$1 ; shift
+        IMPROVE_DATA_DIR=$1 ; shift
         CMD="python ${CANDLE_MODEL}"
         echo "CMD = $CMD"
 
 elif [ $# -ge 3 ] ; then
         CUDA_VISIBLE_DEVICES=$1 ; shift
-        CANDLE_DATA_DIR=$1 ; shift
+        IMPROVE_DATA_DIR=$1 ; shift
 
         # if $3 is a file, then set candle_config
-        if [ -f $CANDLE_DATA_DIR/$1 ] ; then
+        if [ -f $IMPROVE_DATA_DIR/$1 ] ; then
 		echo "$1 is a file"
                 CANDLE_CONFIG=$1 ; shift
                 CMD="python ${CANDLE_MODEL} --config_file $CANDLE_CONFIG $@"
@@ -59,10 +59,10 @@ fi
 
 # Display runtime arguments
 echo "using CUDA_VISIBLE_DEVICES ${CUDA_VISIBLE_DEVICES}"
-echo "using CANDLE_DATA_DIR ${CANDLE_DATA_DIR}"
+echo "using IMPROVE_DATA_DIR ${IMPROVE_DATA_DIR}"
 echo "using CANDLE_CONFIG ${CANDLE_CONFIG}"
 echo "running command ${CMD}"
 
 # Set up environmental variables and execute model
 # source /opt/conda/bin/activate /usr/local/conda_envs/Paccmann_MCA
-CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES} CANDLE_DATA_DIR=${CANDLE_DATA_DIR} $CMD
+CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES} IMPROVE_DATA_DIR=${IMPROVE_DATA_DIR} $CMD
