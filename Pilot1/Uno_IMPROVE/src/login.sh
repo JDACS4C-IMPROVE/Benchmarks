335744aa99421653c3fac3bb9aab51c4646bf2b7
Rylie Weaver <weaverr@lambda0.cels.anl.gov>
2024-01-28 03:02:32 -0800

    move src location

--- Pilot1/Uno_IMPROVE/src/login.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -x
-
-echo "logging into new container"
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-source "${SCRIPT_DIR}/../config/run.config"
-singularity shell --nv --fakeroot --writable $1
