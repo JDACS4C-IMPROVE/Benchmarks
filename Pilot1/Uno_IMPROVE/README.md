64821d096509a663e94f8c1751b7e4e9edd44cfa
Rylie Weaver <weaverr@lambda0.cels.anl.gov>
2024-01-27 19:24:25 -0800

    update def file and instructions

--- Pilot1/Uno_IMPROVE/README.md
+++ Pilot1/Uno_IMPROVE/README.md
@@ -1,13 +1,22 @@
 # Files in the repo
 - Modified for IMPROVE (pick one configuration of drug):
+   - uno_default_model.txt
    - uno_preprocess.py
    - uno_train.py
    - uno_infer.py
-   - EXTRA: uno_baseline_keras.py - keep this file as a master file that calls the above three files
-   - uno_default_model.txt
 
-# Dependencies
-- Install notes: Python=3.8, Tensorflow = 2.11, PyArrow 12.0.1
+# Conda Run (Miniconda version: 23.11.0)
+- conda create Uno_IMPROVE python=3.7.16
+- conda activate Uno_IMPROVE
+- conda config --add channels conda-forge
+- conda install tensorflow-gpu=2.10.0
+- pip install git+https://github.com/ECP-CANDLE/candle_lib@develop
+- git clone https://github.com/JDACS4C-IMPROVE/IMPROVE.git
+- export PYTHONPATH=<IMPROVE_LIBRARY_PATH>/:$PYTHONPATH
+- pip install pyarrow==12.0.1
+- pip install scikit-learn==1.0.2
+- pip install joblib 1.3.2
+
 
 In order to run the modified version of the code, you need to run the following commands:
 ```
@@ -15,5 +24,4 @@ export IMPROVE_DATA_DIR=<UNO_PATH>csa_data or other as per desired directory
 export PYTHONPATH=<IMPROVE_LIBRARY>/:$PYTHONPATH 
 python uno_preprocess.py
 python uno_train.py
-python uno_infer.py
 ```
--- Pilot1/Uno_IMPROVE/uno_utils_improve.py
+++ Pilot1/Uno_IMPROVE/uno_utils_improve.py
@@ -1,3 +1,41 @@
+import numpy as np
+
+
+def data_generator(x_data, y_data, batch_size):
+    """
+    A generalized generator function for creating batches of data.
+
+    :param x_data: NumPy array of features.
+    :param y_data: NumPy array of labels.
+    :param batch_size: Size of the batches to be generated.
+    :return: Yields a tuple (batch_x, batch_y) in each iteration.
+    """
+    num_samples = len(x_data)
+    while True:  # Loop indefinitely
+        for offset in range(0, num_samples, batch_size):
+            # Calculate end of the current batch
+            end = offset + batch_size
+            # Generate batches
+            batch_x = x_data[offset:end]
+            batch_y = y_data[offset:end]
+            # Yield the current batch
+            yield (batch_x, batch_y)
+
+
+def batch_predict(model, data_generator, steps, flatten=True):
+    predictions = []
+    true_values = []
+    for _ in range(steps):
+        x, y = next(data_generator)
+        pred = model.predict(x, verbose=0)
+        if flatten:
+            pred = pred.flatten()
+            y = y.flatten()
+        predictions.extend(pred)
+        true_values.extend(y)
+    return np.array(predictions), np.array(true_values)
+
+
 def print_duration(activity: str, start_time: float, end_time: float):
     duration = end_time - start_time
     hours = int(duration // 3600)
