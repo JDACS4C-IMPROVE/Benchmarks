7db1c1cb51c188c4dfe486619bda9611674d56d9
Rylie Weaver <weaverr@lambda0.cels.anl.gov>
2024-02-01 17:25:39 -0800

    move definitions for hpo compatibility

--- Pilot1/Uno_IMPROVE/uno_preprocess_improve.py
+++ Pilot1/Uno_IMPROVE/uno_preprocess_improve.py
@@ -9,8 +9,8 @@ from uno_utils_improve import print_duration
 
 # Dependencies: pandas, numpy, joblib, scikit-learn
 
-# [Req] Imports from preprocess script
-from uno_train_improve import train_params
+# [Req] Import params
+from params import app_preproc_params, model_preproc_params, app_train_params, model_train_params
 
 import numpy as np
 import pandas as pd
@@ -41,100 +41,8 @@ from improve import (
 # ---------------------
 # [Req] Parameter lists
 # ---------------------
-# Two parameter lists are required:
-# 1. app_preproc_params
-# 2. model_preproc_params
-# 
-# The values for the parameters in both lists should be specified in a
-# parameter file that is passed as default_model arg in
-# frm.initialize_parameters().
-
-# 1. App-specific params (App: monotherapy drug response prediction)
-app_preproc_params = [
-    {
-        "name": "y_data_files",  # default
-        "type": str,
-        "help": "List of files that contain the y (prediction variable) data. \
-             Example: [['response.tsv']]",
-    },
-    {
-        "name": "x_data_canc_files",  # required
-        "type": str,
-        "help": "List of feature files including gene_system_identifer. Examples: \n\
-             1) [['cancer_gene_expression.tsv', ['Gene_Symbol']]] \n\
-             2) [['cancer_copy_number.tsv', ['Ensembl', 'Entrez']]].",
-    },
-    {
-        "name": "x_data_drug_files",  # required
-        "type": str,
-        "help": "List of feature files. Examples: \n\
-             1) [['drug_SMILES.tsv']] \n\
-             2) [['drug_SMILES.tsv'], ['drug_ecfp4_nbits512.tsv']]",
-    },
-    {
-        "name": "canc_col_name",
-        "default": "improve_sample_id",  # default
-        "type": str,
-        "help": "Column name in the y (response) data file that contains the cancer sample ids.",
-    },
-    {
-        "name": "drug_col_name",  # default
-        "default": "improve_chem_id",
-        "type": str,
-        "help": "Column name in the y (response) data file that contains the drug ids.",
-    },
-]
-
-# 2. Model-specific params (Model: Uno)
-model_preproc_params = [
-    {
-        "name": "use_lincs",
-        "type": frm.str2bool,
-        "default": False,
-        "help": "Flag to indicate if using landmark genes.",
-    },
-    {
-        "name": "ge_scaling",
-        "type": str,
-        "default": "std",
-        "choice": ["std", "minmax", "miabs", "robust", "l1", "l2", "max", "power_yj"],
-        "help": "Scaler for gene expression data.",
-    },
-    {
-        "name": "ge_scaler_fname",
-        "type": str,
-        "default": "x_data_gene_expression_scaler.gz",
-        "help": "File name to save the gene expression scaler object.",
-    },
-    {
-        "name": "md_scaling",
-        "type": str,
-        "default": "std",
-        "choice": ["std", "minmax", "miabs", "robust", "l1", "l2", "max", "power_yj"],
-        "help": "Scaler for gene expression data.",
-    },
-    {
-        "name": "md_scaler_fname",
-        "type": str,
-        "default": "x_data_mordred_scaler.gz",
-        "help": "File name to save the Mordred scaler object.",
-    },
-    {
-        "name": "preprocess_debug",
-        "type": bool,
-        "default": False,
-        "help": "Debug mode to show data",
-    },
-    {
-        "name": "preprocess_subset_data",
-        "type": bool,
-        "default": False,
-        "help": "Subsetting data for faster test runs",
-    },
-]
-
 preprocess_params = app_preproc_params + model_preproc_params
-
+train_params = app_train_params + model_train_params
 
 # ------------------------------------------------------------
 
